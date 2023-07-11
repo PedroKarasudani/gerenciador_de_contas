e15a920b44e23445caa5b58d1389bdb9402b78e4
Pedro K. L <pedro.karasudani@hotmail.com>
2023-07-10 21:45:08 -0300

    Colletions - Ordenando uma lista

--- src/br/com/alura/TestandoListas.java
+++ src/br/com/alura/TestandoListas.java
@@ -1,6 +1,7 @@
 package br.com.alura;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TestandoListas {
     public static void main(String[] args) {
@@ -33,5 +34,10 @@ public class TestandoListas {
             System.out.println("Percorrendo: ");
             System.out.println("Aula: " + aula);
         });
+
+        String aula4 = "A primeira roda"; 
+        aulas.add(aula4);
+        Collections.sort(aulas);
+        System.out.println(aulas);
     }
 }

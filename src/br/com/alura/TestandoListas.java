afb26ccb7ec6e887145795ad11c4e21e8b5f7a6f
Pedro K. L <pedro.karasudani@hotmail.com>
2023-07-10 21:03:46 -0300

    ArrayList - Adicionando elemenos

---
+++ src/br/com/alura/TestandoListas.java
@@ -0,0 +1,19 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+
+public class TestandoListas {
+    public static void main(String[] args) {
+        
+        String aula1 = "Modelando a classe Aula";
+        String aula2 = "Conhecedo mais de listas";
+        String aula3 = "Trabalhando com Cursos e Sets";
+
+        ArrayList<String> aulas = new ArrayList<>();
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+
+        System.out.println(aulas);
+    }
+}

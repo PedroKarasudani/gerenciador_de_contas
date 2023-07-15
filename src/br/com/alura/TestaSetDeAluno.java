ec517263d1f2e510ee26aa069988933bc70b1fd4
Pedro K. L <pedro.karasudani@hotmail.com>
2023-07-14 21:25:29 -0300

    HashSet - Ordem aleatoria (sacola de dados) - nao adiciona atributo repetido

--- src/br/com/alura/TestaSetDeAluno.java
+++ src/br/com/alura/TestaSetDeAluno.java
@@ -6,13 +6,24 @@ import java.util.Set;
 public class TestaSetDeAluno {
     public static void main(String[] args) {
         Set<String> alunos = new HashSet<>();
+
         alunos.add("Paulo");
         alunos.add("Alberto");
         alunos.add("Pedro");
         alunos.add("Nico");
+        
+
+        boolean adicionou = alunos.add("Roberto");
+        System.out.println("adiciciou Roberto??" + adicionou);
+        boolean adicionou2 = alunos.add("Nico");
+        System.out.println("adiciciou Nico??" + adicionou2);
+        
+        
 
         for (String aluno : alunos) {
             System.out.println(aluno);            
         }
+        System.out.println(alunos.size());
+
     }
 }

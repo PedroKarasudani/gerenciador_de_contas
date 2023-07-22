14447c292749293961bedb92e33599f1c2680d86
Pedro K. L <pedro.karasudani@hotmail.com>
2023-07-21 22:17:25 -0300

    Map - implemento o Map, para buscar aluno pela matricula

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -1,10 +1,11 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Map;
 import java.util.Set;
 
 public class Curso {
@@ -12,6 +13,7 @@ public class Curso {
     private String instrutor;
     private List<Aula> aulas = new LinkedList<Aula>();
     private Set<Aluno> alunos = new HashSet<>();
+    private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
     
     public Curso(String nome, String instrutor) {
         this.nome = nome;
@@ -51,7 +53,10 @@ public class Curso {
     // }
 
     public void matricula(Aluno aluno){
+        //adiciona no Set de alunos
         this.alunos.add(aluno);
+        //cria a relacao no Map
+        this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
     }
 
     public Set<Aluno> getAlunos() {
@@ -68,11 +73,6 @@ public class Curso {
     }
 
     public Aluno buscaMatricula(int numero) {
-        for (Aluno aluno : alunos) {
-            if(aluno.getNumeroMatricula() == numero){
-                return aluno;
-            }
-        }
-        throw new NoSuchElementException("Matricula: " + numero + " nao encontrada!!");
+        return this.matriculaParaAluno.get(numero);
     }
 }

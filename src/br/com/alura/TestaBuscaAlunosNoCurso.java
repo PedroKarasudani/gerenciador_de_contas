96abcf7392b126a062727ee145e25d781e3b44cc
Pedro K. L <pedro.karasudani@hotmail.com>
2023-07-21 22:21:38 -0300

    Map - AVISO chave tem que ser unica

--- src/br/com/alura/TestaBuscaAlunosNoCurso.java
+++ src/br/com/alura/TestaBuscaAlunosNoCurso.java
@@ -7,11 +7,14 @@ public class TestaBuscaAlunosNoCurso {
         Aluno a1 = new Aluno("Rodrigo Turini", 34672);
         Aluno a2 = new Aluno("Guilherme Silveira", 5617);
         Aluno a5 = new Aluno("Mauricio Aniche", 17645);
+        Aluno a3 = new Aluno("Joao Neto", 5617); //mesmo numero de matricula que a2
 
         javaColecoes.matricula(a1);
         javaColecoes.matricula(a2);
         javaColecoes.matricula(a5);
+        javaColecoes.matricula(a3);
 
+        System.out.println(javaColecoes.getAlunos());
         System.out.println("Quem e o aluno com matricula 5617?");
         Aluno aluno = javaColecoes.buscaMatricula(5617);
         System.out.println("Aluno: " + aluno);        
